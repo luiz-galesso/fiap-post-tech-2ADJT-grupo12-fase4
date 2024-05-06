8c777a1514e54ebd5cecc91dcec394b0038712b4
LarissaGiovanaAlaminos <lari.alaminos@gmail.com>
2024-05-06 20:50:47 -0300

    Ajuste de tratativa Produto Controller

--- ms-catalogo-produtos/src/main/java/com/fase4/techchallenge/fiap/msgerenciamentoprodutos/infrastructure/produto/controller/ProdutoController.java
+++ ms-catalogo-produtos/src/main/java/com/fase4/techchallenge/fiap/msgerenciamentoprodutos/infrastructure/produto/controller/ProdutoController.java
@@ -4,8 +4,6 @@ import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.entity.produto.model
 import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.infrastructure.produto.controller.dto.ProdutoInsertDTO;
 import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.infrastructure.produto.controller.dto.ProdutoUpdateDTO;
 import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.usecase.produto.*;
-import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.usecase.exception.BussinessErrorException;
-import com.fase4.techchallenge.fiap.msgerenciamentoprodutos.usecase.exception.EntityNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
@@ -36,60 +34,41 @@ public class ProdutoController {
     @PostMapping(produces = "application/json")
     @Transactional
     public ResponseEntity<?> create(@RequestBody ProdutoInsertDTO produtoInsertDTO) {
-        try {
-            Produto produto = cadastrarProduto.execute(produtoInsertDTO);
-            return new ResponseEntity<>(produto, HttpStatus.CREATED);
-        } catch (BussinessErrorException bussinessErrorException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bussinessErrorException.getMessage());
-        }
+        Produto produto = cadastrarProduto.execute(produtoInsertDTO);
+        return new ResponseEntity<>(produto, HttpStatus.CREATED);
+
     }
 
     @Operation(summary = "Altera os dados do produto", description = "Serviço utilizado para alterar os dados do produto.")
     @PutMapping(value = "/{id}", produces = "application/json")
     @Transactional
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ProdutoUpdateDTO produtoUpdateDTO) {
-        try {
-            var produtoRetorno = atualizarProduto.execute(id, produtoUpdateDTO);
-            return new ResponseEntity<>(produtoRetorno, HttpStatus.ACCEPTED);
-        } catch (BussinessErrorException bussinessErrorException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bussinessErrorException.getMessage());
-        }
+        var produtoRetorno = atualizarProduto.execute(id, produtoUpdateDTO);
+        return new ResponseEntity<>(produtoRetorno, HttpStatus.ACCEPTED);
     }
 
     @Operation(summary = "Busca o produto pelo Id", description = "Serviço utilizado para buscar o produto pelo Id.")
     @GetMapping(value = "/{id}", produces = "application/json")
     @Transactional
     public ResponseEntity<?> findById(@PathVariable Long id) {
-        try {
-            var produto = obterProdutoPeloId.execute(id);
-            return new ResponseEntity<>(produto, HttpStatus.OK);
-        } catch (EntityNotFoundException entityNotFoundException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(entityNotFoundException.getMessage());
-        }
+        var produto = obterProdutoPeloId.execute(id);
+        return new ResponseEntity<>(produto, HttpStatus.OK);
     }
 
     @Operation(summary = "Remove o produto pelo Id", description = "Serviço utilizado para remover o produto pelo Id.")
     @DeleteMapping(value = "/{id}", produces = "application/json")
     @Transactional
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        try {
-            var produto = removerProdutoPeloId.execute(id);
-            return new ResponseEntity<>("Produto Removido", HttpStatus.OK);
-        } catch (BussinessErrorException bussinessErrorException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bussinessErrorException.getMessage());
-        }
+        var produto = removerProdutoPeloId.execute(id);
+        return new ResponseEntity<>("Produto Removido", HttpStatus.OK);
     }
 
     @Operation(summary = "Retira produto do estoque", description = "Serviço utilizado para alterar o estoque do produto.")
     @PutMapping(value = "/atualiza-estoque/{id}/{quantidade}", produces = "application/json")
     @Transactional
     public ResponseEntity<?> retiraEstoque(@PathVariable Long id, @PathVariable Long quantidade) {
-        try {
-            var produtoRetorno = atualizarEstoqueProduto.execute(id, quantidade);
-            return new ResponseEntity<>(produtoRetorno, HttpStatus.ACCEPTED);
-        } catch (BussinessErrorException bussinessErrorException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bussinessErrorException.getMessage());
-        }
+        var produtoRetorno = atualizarEstoqueProduto.execute(id, quantidade);
+        return new ResponseEntity<>(produtoRetorno, HttpStatus.ACCEPTED);
     }
 
 }
