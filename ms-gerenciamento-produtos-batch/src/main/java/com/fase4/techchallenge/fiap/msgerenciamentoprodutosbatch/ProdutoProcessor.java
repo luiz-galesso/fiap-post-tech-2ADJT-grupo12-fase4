05a80931cbcc462b95b2f6714e86711adfb5b6b8
Thiago Ribeiro <thiagoribeirods@outlook.com>
2024-05-12 11:28:53 -0300

    fixed: remocao de logger

--- ms-gerenciamento-produtos-batch/src/main/java/com/fase4/techchallenge/fiap/msgerenciamentoprodutosbatch/ProdutoProcessor.java
+++ ms-gerenciamento-produtos-batch/src/main/java/com/fase4/techchallenge/fiap/msgerenciamentoprodutosbatch/ProdutoProcessor.java
@@ -9,8 +9,6 @@ public class ProdutoProcessor implements ItemProcessor<Produto, Produto>
 {
     @Override
     public Produto process(Produto item) throws Exception {
-        Logger logger = Logger.getLogger(ProdutoProcessor.class.getName());
-        logger.info(item.getDescricaoProduto());
         item.setDataAtualizacao(LocalDateTime.now());
         return item;
     }
