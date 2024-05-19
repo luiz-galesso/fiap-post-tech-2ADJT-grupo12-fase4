18d2f64a310255aad60590478d61996f7946a165
LUIZ GALESSO <lgalesso@gmail.com>
2024-05-19 17:00:03 -0300

    Ajustando variaveis de ambiente

--- ms-logistica-entregas/src/main/resources/import.sql
+++ ms-logistica-entregas/src/main/resources/import.sql
@@ -1,2 +1,2 @@
 INSERT INTO TB_ORIGEM (ID, LOGRADOURO, NUMERO, BAIRRO, COMPLEMENTO, CEP, CIDADE, ESTADO, REFERENCIA) VALUES (1, 'Avenida dos Elefantes', '2345','Jardim central', null, 9370390, 'São Paulo', 'SP', null);
-insert into tb_entregador (cnpj,nome,quantidade_recursos_disponiveis,situacao,id) values (52123949000183,'Rapidão Entregas',8, 'ATIVO',NEXT VALUE FOR entregador_sequence);
+insert into tb_entregador (cnpj,nome,quantidade_recursos_disponiveis,situacao,id) values (52123949000183,'Rapidão Entregas',8, 'ATIVO',NEXTVAL ('entregador_sequence'));
